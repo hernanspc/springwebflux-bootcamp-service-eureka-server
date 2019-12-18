8c910f604a753486268d53fd83e460d799f5f86c
hernanspc <iphone1198@icloud.com>
2019-12-18 11:00:42 -0500

    first commit

---
+++ src/main/java/com/springwebflux/bootcamp/service/eurekaserver/app/SpringwebfluxBootcampServiceEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.springwebflux.bootcamp.service.eurekaserver.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebfluxBootcampServiceEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebfluxBootcampServiceEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springwebflux/bootcamp/service/eurekaserver/app/SpringwebfluxBootcampServiceEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwebflux.bootcamp.service.eurekaserver.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebfluxBootcampServiceEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
