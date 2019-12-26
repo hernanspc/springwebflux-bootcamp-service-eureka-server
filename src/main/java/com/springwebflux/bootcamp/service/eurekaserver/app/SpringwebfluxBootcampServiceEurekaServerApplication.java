a22d337f79100bdd5cb71cd7c71bd22b6f3b4268
hernanspc <iphone1198@icloud.com>
2019-12-26 18:40:50 -0500

    add

--- src/main/java/com/springwebflux/bootcamp/service/eurekaserver/app/SpringwebfluxBootcampServiceEurekaServerApplication.java
+++ src/main/java/com/springwebflux/bootcamp/service/eurekaserver/app/SpringwebfluxBootcampServiceEurekaServerApplication.java
@@ -1,8 +1,8 @@
 package com.springwebflux.bootcamp.service.eurekaserver.app;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
 @SpringBootApplication
 public class SpringwebfluxBootcampServiceEurekaServerApplication {
 
